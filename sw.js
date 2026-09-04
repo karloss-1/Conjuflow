01bc3ea0bc0c1984506623266aec3ab3a1815c34
karloss-1 <jbw89cvbd9@privaterelay.appleid.com>
2026-09-04 14:26:05 -0600

    Bump ConjuFlow cache for UI behavior update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "conjuflow-v6";
+const CACHE_NAME = "conjuflow-v7";
 const APP_FILES = [
   "./", "./index.html", "./styles.css", "./core.js", "./app.js",
   "./manifest.webmanifest", "./data/conjugations.js",
