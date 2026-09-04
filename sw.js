27bb6b9708c4ec06a61f60b86cf4b838a7845029
karloss-1 <jbw89cvbd9@privaterelay.appleid.com>
2026-09-04 13:33:48 -0600

    Scope service worker cache cleanup to ConjuFlow

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith("conjuflow-") && key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener("fetch", event => {
